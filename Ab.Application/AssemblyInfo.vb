42c3e647527d6f6e94b37df5fce8c7112c93f65f
Dehghani Hamed <dehghani.ha@Ghasemiran.com>
2025-01-20 14:32:07 +0330

    140311011432

--- Ab.Application/AssemblyInfo.vb
+++ Ab.Application/AssemblyInfo.vb
@@ -33,7 +33,7 @@ Imports System.Runtime.InteropServices
 ' 2.8.0 تغییر در فرم انتخاب درخواست در ثبت رسید 
 
 
-<Assembly: AssemblyVersion("10.10.149")>
+<Assembly: AssemblyVersion("10.10.150")>
 
 
 '10.9.3 14010724 Dehghani-   تغییرات کارتابل ضایعات
